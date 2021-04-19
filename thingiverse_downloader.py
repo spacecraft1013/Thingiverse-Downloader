788156668077cc40f1e52b961b9c040b125780b8
Amin Boukari <ambouk3@gmail.com>
2021-04-19 02:59:12 -0400

    Allow multiple thing numbers

--- thingiverse_downloader.py
+++ thingiverse_downloader.py
@@ -1,13 +1,13 @@
-import subprocess
 import urllib.request
 import os
 import zipfile
 import glob
 import argparse
 import requests
+import subprocess
 
 
-def download(thing_number):
+def _download(thing_number):
     thing_link = f"https://www.thingiverse.com/thing:{thing_number}/zip"
 
     r = requests.get(thing_link)
@@ -29,40 +29,41 @@ def download(thing_number):
     return filepath, filename
 
 
-def find_stls(path, filename):
-    file_list = []
+def _find_stls(path, filename):
     if os.path.exists(os.path.join(path, "files")):
         path = os.path.join(path, "files")
 
     elif os.path.exists(os.path.join(path, filename.rstrip(".zip"))):
         path = os.path.join(path, filename.rstrip(".zip"))
 
-    for filepath in glob.glob(os.path.join(path, "*.stl")):
-        file_list.append('"' + filepath + '"')
+    file_list = [f'"{path}"' for path in glob.glob(os.path.join(path, "*.stl"))]
 
     return file_list
 
 
-parser = argparse.ArgumentParser(
-    description='Downloads, extracts, and opens STL files from thingiverse')
-parser.add_argument('thing_number', type=int,
-                    help='Thing number from Thingiverse')
-parser.add_argument(
-    '-m', '--mass', help='Whether or not to save to mass production files', action="store_true")
-parser.add_argument(
-    '--no-cura', help='Specify to not open cura after downloading files', action="store_true")
+parser = argparse.ArgumentParser(description='Downloads, extracts, and opens STL files from thingiverse')
+parser.add_argument('thing_numbers', nargs='+', metavar='N', help='Thing numbers from Thingiverse')
+parser.add_argument('-s', '--section', type=str, metavar='S', help='What subfolder to download the file to', required=False)
+parser.add_argument('-m', '--mass', help='Whether or not to save to mass production files', action="store_true")
+parser.add_argument('--no-cura', help='Specify to not open cura after downloading files', action="store_true")
 args = parser.parse_args()
 
-if args.mass == True:
+if args.mass:
     SAVE_PATH = "F:/3D Prints/Mass production"
+
+elif args.section:
+    SAVE_PATH = f"F:/3D Prints/In progress or not printed/{args.section}"
+    if not os.path.exists(SAVE_PATH):
+        os.mkdir(SAVE_PATH)
 
 else:
     SAVE_PATH = "F:/3D Prints/In progress or not printed"
 
-filepath, filename = download(args.thing_number)
+for thing_number in args.thing_numbers:
+    filepath, filename = _download(thing_number)
 
-if args.no_cura == False:
-    stls = find_stls(filepath, filename)
-    stl_list = ' '.join(stls)
-    print("Starting Cura...")
-    os.system('cura ' + stl_list)
+    if not args.no_cura:
+        stls = _find_stls(filepath, filename)
+        command = 'cura ' + ' '.join(stls)
+        print("Starting Cura...")
+        subprocess.Popen(command)
